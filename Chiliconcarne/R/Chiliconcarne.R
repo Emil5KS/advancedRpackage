49c3f6c28c85ea87434c1e7e24d0aa3a75f89d88
Emil5KS <emisv463@student.liu.se>
2016-09-13 15:30:43 +0200

    Updated the package and functions descriptions

---
+++ Chiliconcarne/R/Chiliconcarne.R
@@ -0,0 +1,9 @@
+#' A package created for a series of labs for a course in Advanced R-programming 
+#'
+#'
+#'
+#'
+#'
+#'@docType package 
+#'@name Chiliconcarne
+NULL
--- Chiliconcarne/R/euclidian.R
+++ Chiliconcarne/R/euclidian.R
@@ -1,8 +1,32 @@
+#' Computes the greatest common devisor 
+#' 
 #' Computes the greatest common devisor of two numeric integers via
 #' the Euclidian algorithm. The implementation is division based and uses the
 #' modulus function.
 #' 
+#' @param s    an integer
+#' @param b    an integer
+#' @return The greatest common devisor of \code{s} and \code{b}.
 #' 
+#' @details 
+#' The algorithm uses a while statement and the modulus function to find the largest number
+#' that divides both of them without leaving a remainder.
+#' The remainder and the smallest value from the
+#'  previous iteration is stored in every iteration.
+#'  When the remainder equals zero, the iterations stops and the function return 
+#'  the greatest common devisor. 
+#'  
+#' @references 
+#'
+#' http://en.wikipedia.org/wiki/Euclidean_algorithm
+#' 
+#' @examples 
+#' 
+#' ## Compute the common devisor of 123612 and 13892347912
+#' euclidian(123612,13892347912)
+#' 
+#' ## Compute the common devisor of 100 and 1000
+#' euclidian(100,1000)
 #' 
 #' @export
 
