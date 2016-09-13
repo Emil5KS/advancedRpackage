ffe313ed101da2d1763daa249cf641d87edc5eba
Emil5KS <emisv463@student.liu.se>
2016-09-13 16:39:05 +0200

    Added some description about the dataset that's included in the package.

---
+++ Chiliconcarne/R/data.R
@@ -0,0 +1,10 @@
+#' The dataset wiki_graph
+#' 
+#' wiki_graph is a data.frame with three variables, v1, v2 and w. The data was used in testing a 
+#' optional function implementing Dijkstras algorithm 
+#' 
+#'
+#'@source http://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+#'
+#'
+"wiki_graph"
