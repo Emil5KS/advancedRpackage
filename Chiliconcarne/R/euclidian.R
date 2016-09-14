6216a722bbdf4fe84682342e1915d05dc705ecf9
Emil5KS <emisv463@student.liu.se>
2016-09-14 14:14:21 +0200

    Added som chiliconcarnetests

--- Chiliconcarne/R/euclidian.R
+++ Chiliconcarne/R/euclidian.R
@@ -32,13 +32,13 @@
 
 euclidian<-function(s, b){
   
-  try(if(!is.numeric(s)) stop("s is not numeric"))
-  try(if(!is.numeric(b)) stop("b is not numeric"))
+  if(!is.numeric(s)) stop("s is not numeric")
+  if(!is.numeric(b)) stop("b is not numeric")
   
-  try(if(length(s)>1) stop("Scalar values only accepted"))
-  try(if(length(s)<1) stop("Scalar values only accepted"))
-  try(if(length(b)>1) stop("Scalar values only accepted"))
-  try(if(length(b)<1) stop("Scalar values only accepted"))
+  if(length(s)>1) stop("Scalar values only accepted")
+  if(length(s)<1) stop("Scalar values only accepted")
+  if(length(b)>1) stop("Scalar values only accepted")
+  if(length(b)<1) stop("Scalar values only accepted")
   
   if((!s %% 1 == 0) || (!b %% 1 == 0 )){
     stop("Your numbers have to be integers!")
--- Chiliconcarne/tests/testthat/test_chilitests.R
+++ Chiliconcarne/tests/testthat/test_chilitests.R
@@ -1,8 +1,9 @@
-test_that("concarnetestet ",{expect_equal(euclidian(100,1000),100)})
-test_that("concarnetestet ",{expect_equal(euclidian(123612,13892347912),4)})
-test_that("concarnetestet ",{expect_warning(euclidian("TUUUSEN",13892347912),4)})
-test_that("concarnetestet ",{expect_warning(euclidian(100,"Swedish Husman"))})
-test_that("concarnetestet ",{expect_warning(euclidian( c(22,41),c(12,31))) })
+test_that("concarnetestet1 ",{expect_equal(euclidian(100,1000),100)})
+test_that("concarnetestet2 ",{expect_equal(euclidian(123612,13892347912),4)})
+test_that("concarnetestet3 ",{expect_error(euclidian("TUUUSEN",13892347912))})
+test_that("concarnetestet4 ",{expect_error(euclidian(100,"Swedish Husman"))})
+test_that("concarnetestet5 ",{expect_error(euclidian( c(22,41),c(12,31))) })
+test_that("concarnetestet6 ",{expect_error(euclidian(TRUE,1000))})
+test_that("concarnetestet7 ",{expect_error(euclidian(data.frame(matrix(c(1:100),ncol=5)),1000))})
+test_that("concarnetestet8 ",{expect_error(euclidian(1.5,1000))})
 
-
-expect_warning()
